d7e01f9fd6f553340bb07f2af49491110a50061a
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-03-14 12:49:26 +0100

    Program.cs method tests

--- Motors/Program.cs
+++ Motors/Program.cs
@@ -4,7 +4,42 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Statisztika statisztika = new Statisztika();
+
+			string filename = "motors.txt"; 
+			statisztika.ReadFromFile(filename);
+
+            Console.WriteLine($"Összes motor összértéke: {statisztika.SumPrices()} EUR");
+
+            Console.WriteLine(); //////////////////////////////////
+
+            string valasztottMotor = "Royal Enfield";
+			if ( ( statisztika.Contains(valasztottMotor) ) == true )
+			{
+                Console.WriteLine($"Van {valasztottMotor} a listában.");
+            } 
+			else
+			{
+				Console.WriteLine($"{valasztottMotor} nincs a listában.");
+			}
+
+			Console.WriteLine(); //////////////////////////////////
+
+			Console.WriteLine($"A választott {valasztottMotor} brandű motorok kollektív összege: {statisztika.SumBasedOnBrand(valasztottMotor, filename)}");
+
+			Console.WriteLine(); //////////////////////////////////
+
+			Console.WriteLine($"A legidősebb motor: {statisztika.Oldest()}");
+
+			Console.WriteLine(); //////////////////////////////////
+
+			statisztika.Sort();
+			Console.WriteLine("Teljesítmény alapján sorbarendezés:");
+			foreach (var motor in statisztika.motors)
+			{
+				Console.WriteLine($"Motor: {motor.Name}, Performance: {motor.Performance} HP");
+
+			}
 		}
 	}
 }
